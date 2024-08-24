6151dfadf2c31642074e766c6f3c503806c06433
Thanmai-05 <jalakamthanmai05@gmail.com>
2024-08-24 12:46:56 +0530

    committing changes made in App.css    src/App.js    src/styles/AddWidgetModal.css    src/styles/Search.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Dashboard from './components/Dashboard';
 import Search from './components/Search';
+import AddWidgetBar from './components/AddWidgetBar';
 import './App.css';
 
 function App() {
+  const [isAddWidgetModalOpen, setAddWidgetModalOpen] = useState(false);
+
+  
+  const openAddWidgetModal = () => {
+    setAddWidgetModalOpen(true);
+    console.log("ttt",isAddWidgetModalOpen);
+  };
+
+  const closeAddWidgetModal = () => {
+    setAddWidgetModalOpen(false);
+  };
+
+ 
+
+  
+
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <h1>CNAPP Dashboard</h1>
-          <Search />
+          <div className="top-bar-actions">
+            <button onClick={openAddWidgetModal} className="add-widget-bar-button">
+              Add Widget
+            </button>
+            <Search />
+          </div>
+          
         </header>
         <Dashboard />
+        {isAddWidgetModalOpen && (
+          <AddWidgetBar
+            closeModal={closeAddWidgetModal}
+            
+          />
+        )}
       </div>
     </Provider>
   );
